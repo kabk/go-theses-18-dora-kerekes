2a44f971a9e70126167568ff10e772e7c5715562
Dora Kerekes <krks.dori@gmail.com>
2018-01-15 21:33:39 +0100

    added some imgs

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 window.onload = function(){
 
         var arrayOfCanvas = [];
-         var imgsArray = [
+        var imgsArray = [
             "imgs/title.png",
             "imgs/chapter-1.png",
             "imgs/chapter-2.png",
@@ -13,18 +13,30 @@ window.onload = function(){
             "imgs/chapter-5.png",
             "imgs/conclusion.png",
         ];
+        var imgsArrayPrint = [
+            "imgs/print-imgs/p-title.jpg",
+            "imgs/print-imgs/p-chapter-1.jpg",
+            "imgs/print-imgs/p-chapter-2.jpg",
+            "imgs/print-imgs/p-chapter-3.jpg",
+            "imgs/print-imgs/p-chapter-4.jpg",
+            "imgs/print-imgs/p-chapter-5.jpg",
+            "imgs/print-imgs/p-conclusion.jpg",
+        ];
 
-		$('.page').each(function(i) {
-			$(this).css('min-height', $(window).outerHeight());
+        $('.page').each(function(i) {
+            $(this).css('min-height', $(window).outerHeight());
         });
 
         var index = 0;
         
         $('.page').each(function(i) {
-            
-        
             if($(this).find("canvas").length > 0) {
                 var c = $(this).find("canvas")[0];
+
+                var img = document.createElement("img");
+                img.src = imgsArrayPrint[index];
+                img.setAttribute("class", "hiddenImg");
+                $(c).parent().append(img);
                 
                 var p = $(this);
                 var position = p.position();
@@ -40,28 +52,28 @@ window.onload = function(){
             }
         });
 
-		var totalHeight = $(document).outerHeight();
-		$('body').css('height', totalHeight);
+        var totalHeight = $(document).outerHeight();
+        $('body').css('height', totalHeight);
 
-		var arrayOfHeights = [];
+        var arrayOfHeights = [];
         var newArray = [];
        
-		$('.page').each(function(i) {
-			if(i != 0) {
-				$(this).css('position', "fixed");
-				$(this).css('top', "0px");
-				$(this).css('left', "0px");
-				$(this).css('z-index', -i);
-			}
-		});
+        $('.page').each(function(i) {
+            if(i != 0) {
+                $(this).css('position', "fixed");
+                $(this).css('top', "0px");
+                $(this).css('left', "0px");
+                $(this).css('z-index', -i);
+            }
+        });
 
-		// set scroll space
-		
+        // set scroll space
+        
 
-		$('.page').each(function() {
-			//console.log($(this).height());
-			arrayOfHeights.push($(this).outerHeight());
-		});
+        $('.page').each(function() {
+            //console.log($(this).height());
+            arrayOfHeights.push($(this).outerHeight());
+        });
 
         
         // snapping pionts
@@ -72,55 +84,55 @@ window.onload = function(){
             newArray.push(sum);
         }
 
-		$( window ).scroll(function() {
-  			//console.log($(this).scrollTop());
+        $( window ).scroll(function() {
+            //console.log($(this).scrollTop());
 
-  			var scrollValue = $(this).scrollTop();
+            var scrollValue = $(this).scrollTop();
 
-  			global_selection = returnSelectedPage(newArray, scrollValue, arrayOfHeights);
+            global_selection = returnSelectedPage(newArray, scrollValue, arrayOfHeights);
 
-  			$('.page').each(function(i) {
+            $('.page').each(function(i) {
 
 
-  					if(i < global_selection) {
-  						$(this).css('position', "relative");
+                    if(i < global_selection) {
+                        $(this).css('position', "relative");
                         //$(this).css('visibility', "hidden");
-  					}
+                    }
 
-  					if(i == global_selection) {
-  						$(this).css('position', "relative");
+                    if(i == global_selection) {
+                        $(this).css('position', "relative");
                         //$(this).css('visibility', "visible");
-  					}
+                    }
 
-  					if(i > global_selection) {
-  						$(this).css('position', "fixed");
+                    if(i > global_selection) {
+                        $(this).css('position', "fixed");
                           //$(this).css('visibility', "visible");
-  					}
+                    }
 
                     if(i > global_selection+1) {
                          //$(this).css('visibility', "hidden");
                     }
 
-			});
+            });
 
-		});
+        });
 
-		function returnSelectedPage(newArray, scrollValue, arrayOfHeights) {
-			
-			var selection = 0;
-			for(var n in newArray) {
-				if(scrollValue >= newArray[n] && scrollValue < newArray[(parseInt(n)+1)]) {
-					selection = n;
-				}
-			}
-			
-			return selection;
-		}
+        function returnSelectedPage(newArray, scrollValue, arrayOfHeights) {
+            
+            var selection = 0;
+            for(var n in newArray) {
+                if(scrollValue >= newArray[n] && scrollValue < newArray[(parseInt(n)+1)]) {
+                    selection = n;
+                }
+            }
+            
+            return selection;
+        }
 
 
-		//-----------------------------------------------------------------
+        //-----------------------------------------------------------------
         
-    		 $('canvas').each(function(index) {
+             $('canvas').each(function(index) {
                         var canvas = this;
 
                         var rows = 30;
@@ -224,4 +236,3 @@ function render(arrayOfCanvas, img, rows, cols, pieces, pieceWidth, pieceHeight)
                     render(arrayOfCanvas, img, rows, cols, pieces, pieceWidth, pieceHeight)
                 });
         };
-
